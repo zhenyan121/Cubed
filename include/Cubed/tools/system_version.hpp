296bd8e07a8e61a471529a1c1e56fb65cc1e43cb
zhenyan121 <3367366583@qq.com>
2026-04-16 17:20:42 +0800

    feat: add os version show for windows

--- include/Cubed/tools/system_version.hpp
+++ include/Cubed/tools/system_version.hpp
@@ -1,13 +1,37 @@
 #pragma once
 
 #include <string>
-#include <fstream>
-
 #include <Cubed/tools/log.hpp>
 
 #ifdef _WIN32
+#include <windows.h>
+typedef LONG (WINAPI* RtlGetVersionPtr)(PRTL_OSVERSIONINFOW);
+inline bool get_os_version(std::string& str) {
+    HMODULE hntdll = GetModuleHandleW(L"ntdll.dll");
+    if (!hntdll) return false;
+
+    auto prtl_get_version = reinterpret_cast<RtlGetVersionPtr>(
+        GetProcAddress(hntdll, "RtlGetVersion"));
+    if (!prtl_get_version) return false;
+
+    RTL_OSVERSIONINFOW osvi = { 0 };
+    osvi.dwOSVersionInfoSize = sizeof(osvi);
+    if (prtl_get_version(&osvi) != 0) return false;
+    if (osvi.dwMajorVersion == 10) {
+        if (osvi.dwBuildNumber >= 22000) {
+            str = "Windows 11 Build " + std::to_string(osvi.dwBuildNumber);
+        } else {
+            str = "Windows 10 Build " + std::to_string(osvi.dwBuildNumber);
+        }
+    } else {
+        str = "Windows Build " + std::to_string(osvi.dwBuildNumber);
+    }
+    return true;
+}
 
 #elif defined(__linux__)
+#include <fstream>
+
 inline bool get_os_version(std::string& str) {
     std::ifstream file("/etc/os-release");
     if (!file.is_open()) {
@@ -31,6 +55,11 @@ inline bool get_os_version(std::string& str) {
             return true;
         }
     }
+    return false;
+}
+#else
+inline bool get_os_version(std::string& str) {
+    str = "Unknown OS";
     return false;
 }
 #endif
