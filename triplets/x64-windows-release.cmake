b938bfacf12ad3a89698229380c90d4facac5ede
zhenyan121 <3367366583@qq.com>
2026-07-02 15:39:44 +0800

    refactor(triplets): set arch and linkage explicitly in x64-windows-release

--- triplets/x64-windows-release.cmake
+++ triplets/x64-windows-release.cmake
@@ -1,3 +1,5 @@
-include("${VCPKG_ROOT}/triplets/x64-windows.cmake")
+set(VCPKG_TARGET_ARCHITECTURE x64)
+set(VCPKG_CRT_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE dynamic)
 
 set(VCPKG_BUILD_TYPE release)
